2da09c489d7be754e30d1e953ba9e74c14a4708c
mutsuki <mutsuki@momo86.net>
2019-08-16 09:05:32 +0900

    Update test

--- test/main.cpp
+++ test/main.cpp
@@ -5,4 +5,10 @@ int main() {
 	mtk::ptcsv ptcsv;
 
 	ptcsv.load_csv("test.csv");
+
+	for(const auto& row : ptcsv.get_rows()) {
+		for(const auto &item : row) {
+			std::printf("%15s : %s\n", item.first.c_str(), item.second.c_str());
+		}
+	}
 }
